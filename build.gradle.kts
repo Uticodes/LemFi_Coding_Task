d86aacfcac06eabfa88f91f49252bc95593e1a20
UtibeEtim <etim.utibe@gmail.com>
2024-03-02 23:56:07 +0100

    Update jvmTarget to 17 to support running all test cases

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_11.toString()
+            jvmTarget = JavaVersion.VERSION_17.toString()
         }
     }
 
